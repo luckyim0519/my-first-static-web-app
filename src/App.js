5394da8e2e8b645b32dadb92d8be916d3aae3ab7
luckyim0519 <moon990519@gmail.com>
2023-11-15 21:38:19 -0500

    test2-updates-working?

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'World';
-  return <div>Hello Bye {value} some update</div>;
+  return <div>Hello Bye {value} some update !! why not updating!!!!! HELP!!!!</div>;
 }
 
 export default App;
