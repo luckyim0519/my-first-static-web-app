57157cadda4657ed791033e48d83c2ba3c987516
Shathurshika Chandrakumar <74083424+shath1955@users.noreply.github.com>
2023-11-15 22:55:10 -0500

    shathi commit test 2

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
   const value = 'help me';
-  return <div>heya this is shathi</div>;
+  return <div>heya this is shathi part2</div>;
 }
 
 export default App;
