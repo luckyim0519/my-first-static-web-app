2dcb5de20741db8a8023df01d1ebc301667ada1f
luckyim0519 <moon990519@gmail.com>
2023-11-25 12:03:39 -0500

    testing speech

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from 'react';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import SpeechHome from './components/SpeechHome';
+s
 function App() {
   const [data, setData] = useState('');
-
   useEffect(() => {
     (async function () {
       const { text } = await( await fetch(`/api/message`)).json();
@@ -10,7 +11,14 @@ function App() {
     })();
   });
 
-  return <div>{data}</div>;
+  return (
+    <Router>
+      <div>{data}</div>
+      <Route path="/SpeechHome">
+        <SpeechHome />
+      </Route>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/SpeechHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SpeechHome = () => {
+  return (
+    <div>
+      <h1>Speech Home</h1>
+      <h2>need to set up the virtual speech sdk</h2>
+    </div>
+  );
+};
+
+export default SpeechHome;
