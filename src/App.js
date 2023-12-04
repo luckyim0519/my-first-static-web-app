de6d81b4d831564581eb0a052dd7a8e38d06b08a
luckyim <moon990519@gmail.com>
2023-12-03 19:17:08 -0500

    push change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import SpeechHome from './components/SpeechHome';
 
 function App() {
   const [data, setData] = useState('');
 
-  return (
-    <Router>
-      <div>
-        <h1>Hello, this is our AI classroom</h1>
-        <Link to="/SpeechHome">SpeechHome</Link>
-      </div>
-      <div>Hello?</div>
-      <Route path="SpeechHome">
-        <SpeechHome />
-      </Route>
-    </Router>
-  );
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/message`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
